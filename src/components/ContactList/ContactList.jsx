feb1492fb23e3c719689b2eaca0d255d5b0de603
MarinaSigida <sigida.web@gmail.com>
2023-09-10 12:50:37 +0200

    start-point

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectFilteredContacts } from '../../redux/filter/filter-selectors';
+import { deleteContactsThunk } from '../../redux/contacts/contacts-thunk';
+import {getContactsThunk} from '../../redux/contacts/contacts-thunk';
+import ContactItem from '../ContactItem/ContactItem';
+import style from './ContactList.module.css';
+
+const ContactList = () => {
+  const contacts = useSelector(selectFilteredContacts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+   
+    dispatch(getContactsThunk());
+  }, [dispatch]);
+
+  const handleContactDelete = (id) => {
+    dispatch(deleteContactsThunk(id));
+  };
+
+  
+  return (
+    <ul className={style.list}>
+      {contacts.length ? (
+        contacts.map(({ id, name, number }) => {
+          return (
+            <ContactItem
+              key={id}
+              id={id}
+              name={name}
+              number={number}
+              onDelete={() => handleContactDelete(id)}
+            />
+          );
+        })
+      ) : (
+        <li className={style.error}>The list is empty</li>
+      )}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default ContactList;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectFilter } from '../../redux/filter/filter-selectors';
+import { filterContactAction } from '../../redux/filter/filter-slice';
+import style from './Filter.module.css';
+
+
+const Filter = () => {
+  const filterValue = useSelector(selectFilter);
+  const dispatch = useDispatch();
+
+  return (
+  <div className={style.searchContainer}>
+    <label className={style.search}>
+      <input
+        type="text"
+        name="filter"
+        placeholder=" "
+        className={style.inputName}
+        title="Enter search name"
+        onChange={(e)=> dispatch(filterContactAction(e.target.value))}
+        value={filterValue}
+      />
+    </label>
+  </div>)
+};
+
+
+export default Filter;
